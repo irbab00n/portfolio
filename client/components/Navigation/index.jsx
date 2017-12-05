25e89b9804c92e6e551824f789f1d7fd1a395f26
Thomas Cosby <cosbyts@gmail.com>
2017-12-04 16:50:37 -0800

    (feat) Dropdown menu created
    Currently works with mock data representing items in the list.
    
    Todo:
    
    Refactor the component into a separate file.

--- client/components/Navigation/index.jsx
+++ client/components/Navigation/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import _ from 'lodash';
 
 import NavLink from './NavLink.jsx';
 
@@ -11,46 +10,82 @@ class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dropdownToggled: false,
-
+      dropdownToggle: false,
     };
     this.toggleDropdown = this.toggleDropdown.bind(this);
   }
 
-  toggleDropdown() {
-    let { dropdownToggled } = this.state;
-    this.setState({dropdownToggled: !dropdownToggled});
+  toggleDropdown(currentFlag) {
+    this.setState({dropdownToggle: !currentFlag});
   }
 
   render() {
 
-    const { dropdownToggled } = this.state;
+    const { dropdownToggle } = this.state;
     const { percentScrolled, screenWidth } = this.props;
     let currentScroll = {
       backgroundColor: `rgba(250, 250, 250, ${(percentScrolled / 10).toFixed(1)})`, 
       boxShadow: `0 4px 8px 0 rgba(0, 0, 0, ${(percentScrolled / 100).toFixed(2) >= 0.4 ? 0.4 : (percentScrolled / 100).toFixed(2)})`
     };
-    let toggleMobile = screenWidth <= 700;
+    let mobileToggle = screenWidth <= 700;
 
     return (
 
       <div style={applyStyles(style.main, currentScroll)}>
+
         <span style={style.title_left}>COS<span style={style.title_right}>-BYTES</span></span>
-        {
-          toggleMobile
-          ? <div 
-              style={applyStyles(style.select_button, dropdownToggled && {backgroundColor: 'white', height: '500px',}, )}
-              onClick={this.toggleDropdown}
-            >
-              Hi
-            </div>
-        
-          : <div style={style.navigation_body}>
-              <NavLink name={'Resume'} onClick={() => { console.log('Resume link clicked'); }}/>
-              <NavLink name={'Projects'} onClick={() => { console.log('Projects link clicked'); }}/>
-              <NavLink name={'Blog'} onClick={() => { console.log('Blog link clicked'); }}/>
-            </div>
-        }
+
+      {/* This is where the Dropdown Menu Starts */}
+
+        <div style={{
+          alignSelf: 'flex-start',
+          marginTop: '10px',
+          width: '150px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+          WebkitTransition: '0.2s'
+        }}
+          onClick={() => { this.toggleDropdown(dropdownToggle); }}
+        >
+          <div id='current-selected' style={{
+            height: '50px',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: 'white'
+          }}>
+            
+            {
+              dropdownToggle
+                ? 'Open'
+                : 'Closed'
+            }
+
+          </div>
+
+          <div id='dropdown-list-body' style={applyStyles({display: 'hidden', height: '0px', width: '100%', backgroundColor: 'white', WebkitTransition: 'height 0.2s'}, dropdownToggle && {display: 'flex', height: '225px'})}>
+
+                  <div style={applyStyles({display: 'hidden', flexDirection: 'column', alignItems: 'center', WebkitTransition: '0.2s'}, dropdownToggle && {display: 'flex'})}>
+                    <span style={applyStyles({height: '75px', width: '100%', display: 'hidden', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', WebkitTransition: '0.2s'}, dropdownToggle && {display: 'flex'})}>{dropdownToggle ? 'Item 1' : ''}</span>
+                    <span style={applyStyles({height: '75px', width: '100%', display: 'hidden', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', WebkitTransition: '0.2s'}, dropdownToggle && {display: 'flex'})}>{dropdownToggle ? 'Item 2' : ''}</span>
+                    <span style={applyStyles({height: '75px', width: '100%', display: 'hidden', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', WebkitTransition: '0.2s'}, dropdownToggle && {display: 'flex'})}>{dropdownToggle ? 'Item 3' : ''}</span>
+                  </div>
+          </div>
+
+        </div>
+
+      {/* This is where the Dropdown Menu Ends */}
+    
+        <div style={style.navigation_body}>
+          <NavLink name={'Resume'} onClick={() => { console.log('Resume link clicked'); }}/>
+          <NavLink name={'Projects'} onClick={() => { console.log('Projects link clicked'); }}/>
+          <NavLink name={'Blog'} onClick={() => { console.log('Blog link clicked'); }}/>
+        </div>
+
       </div>
 
     );
--- client/components/Navigation/navbarStyle.js
+++ client/components/Navigation/navbarStyle.js
@@ -24,16 +24,15 @@ module.exports.title_right = {
 module.exports.select_button = {
   alignSelf: 'flex-start',
   display: 'flex',
-  flexDirection: 'row',
+  flexDirection: 'column',
   alignItems: 'center',
-  justifyContent: 'center',
+  justifyContent: 'flex-end',
   height: '36px',
   width: '100px',
-  padding: '17px',
+  margin: '17px 50px 17px',
   backgroundColor: 'rgb(179, 227, 255)',
   borderWidth: '2px',
   borderRadius: '2px',
-  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
   WebkitTransition: 'height 0.4s',
 };
 
