6830f9a68cd6ce9ee3a67b5617d0b3c56378d513
Thomas Cosby <cosbyts@gmail.com>
2017-12-06 15:06:13 -0800

    Updated webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,10 @@ module.exports = {
       {
         test: /\.(js||jsx)?/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel',
+        query: {
+          presets:: ['env', 'react']
+        }
       },
       {
         test: /\.json?/,
