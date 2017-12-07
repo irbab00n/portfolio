68b1213de9d2bb8e2ac0b3fa4ed8b8212c70bba7
Thomas Cosby <cosbyts@gmail.com>
2017-12-06 18:22:30 -0800

    Replaced hosted port number

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Portfolio listening on port ${port}`);
