907b3cef9fb729a11a18ce0a472e94ec1634434b
Thomas Cosby <cosbyts@gmail.com>
2017-12-09 17:47:17 -0800

    (feat) Jumbotron with animations
    Jumbotron now has animated images that will scroll to the sides as the user scrolls down the page.
    
    Re-titled the page.

--- client/components/Jumbotron/index.jsx
+++ client/components/Jumbotron/index.jsx
@@ -11,13 +11,40 @@ class Jumbotron extends React.Component {
 
   render() {
 
-    const { percentScrolled } = this.props;
+    const { percentScrolled, yOffset } = this.props;
 
     return (
 
       <div style={applyStyles(style.body, {color: `rgba(41, 67, 78, ${(1 - (percentScrolled / 7))})`})}>
+
+        <div style={{
+          marginRight: `${-(yOffset * 1.11)}px`,
+          zIndex: '-1',
+          marginTop: '-400px',
+          height: '100px',
+          width: '500px',
+          alignSelf: 'flex-end',
+          WebkitTransition: '0.1s'
+        }}>
+          <img src='http://pngimg.com/uploads/cloud/cloud_PNG24.png?i=1'/>
+        </div>
+
+
+        {/* Change where the color applies from the container to the text field */}
         {/* Install a timer that will change this message once every few seconds */}
-        Welcome to my professional programming paradise
+        <span style={{marginBottom: '-400px'}}>Welcome to my professional programming paradise</span>
+
+        <div style={{
+          marginLeft: `${-(yOffset * 1.11)}px`,
+          zIndex: '-1',
+          height: '100px',
+          width: '500px',
+          alignSelf: 'flex-start',
+          WebkitTransition: '0.1s'
+        }}>
+          <img src='https://reclay-group.com/de/images/header/stage_cloud_2.png'/>
+        </div>
+
       </div>
 
     );
--- client/components/Jumbotron/style.js
+++ client/components/Jumbotron/style.js
@@ -1,7 +1,10 @@
 module.exports.body = {
   display: 'flex',
+  zIndex: '-2',
+  overflow: 'hidden',
+  flexDirection: 'column',
   alignItems:'center',
-  justifyContent: 'center',
+  justifyContent: 'space-evenly',
   height: '500px',
   width: '100%',
   background: `linear-gradient(to bottom, #ffffff 6%, #b3e3ff 68%)`,
