1a74279063467c6e959bf72eec4f72fdfc4104bf
Thomas Cosby <cosbyts@gmail.com>
2017-12-09 18:04:05 -0800

    (refactor) Cloud sprites into separate files

--- client/components/Jumbotron/index.jsx
+++ client/components/Jumbotron/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import LeftCloud from './LeftCloud.jsx'
+import RightCloud from './RightCloud.jsx'
+
 import applyStyles from '../../helpers/applyStyles';
 
 import style from './style';
@@ -17,33 +20,11 @@ class Jumbotron extends React.Component {
 
       <div style={applyStyles(style.body, {color: `rgba(41, 67, 78, ${(1 - (percentScrolled / 7))})`})}>
 
-        <div style={{
-          marginRight: `${-(yOffset * 1.11)}px`,
-          zIndex: '-1',
-          marginTop: '-400px',
-          height: '100px',
-          width: '500px',
-          alignSelf: 'flex-end',
-          WebkitTransition: '0.1s'
-        }}>
-          <img src='http://pngimg.com/uploads/cloud/cloud_PNG24.png?i=1'/>
-        </div>
-
-
-        {/* Change where the color applies from the container to the text field */}
-        {/* Install a timer that will change this message once every few seconds */}
+        <RightCloud yOffset={yOffset} />
+        
         <span style={{marginBottom: '-400px'}}>Welcome to my professional programming paradise</span>
 
-        <div style={{
-          marginLeft: `${-(yOffset * 1.11)}px`,
-          zIndex: '-1',
-          height: '100px',
-          width: '500px',
-          alignSelf: 'flex-start',
-          WebkitTransition: '0.1s'
-        }}>
-          <img src='https://reclay-group.com/de/images/header/stage_cloud_2.png'/>
-        </div>
+        <LeftCloud yOffset={yOffset} />
 
       </div>
 
--- client/components/Jumbotron/style.js
+++ client/components/Jumbotron/style.js
@@ -10,3 +10,20 @@ module.exports.body = {
   background: `linear-gradient(to bottom, #ffffff 6%, #b3e3ff 68%)`,
   fontSize: '30px',
 };
+
+module.exports.rightCloud = {
+  zIndex: '-1',
+  marginTop: '-400px',
+  height: '100px',
+  width: '500px',
+  alignSelf: 'flex-end',
+  WebkitTransition: '0.1s'
+};
+
+module.exports.leftCloud = {
+  zIndex: '-1',
+  height: '100px',
+  width: '500px',
+  alignSelf: 'flex-start',
+  WebkitTransition: '0.1s'
+};
