e8b8c4a16ca63e743d20ec16c4fd3eee80c3dcf7
Thomas Cosby <cosbyts@gmail.com>
2017-12-14 16:22:14 -0800

    Default Content and animation
    - Filled out the Default section with a small blurb about myself and how I got into programming.
    
    - Added a sliding animation for when the slide is transitioned to.

--- client/components/Resume/DesktopView/DisplayBox/Slides/Default/index.jsx
+++ client/components/Resume/DesktopView/DisplayBox/Slides/Default/index.jsx
@@ -5,20 +5,41 @@ import style from './style';
 class DefaultSlide extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loaded: false
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loaded: true
+      });
+    }, 100)
   }
 
   render() {
 
+    const { loaded } = this.state;
     const { applyStyles } = this.props;
 
     return (
 
       <div style={
         applyStyles(
-          style.body
+          style.body,
+          loaded && style.body_loaded
         )
       }>
-        <center>This is the default view.  This will be open by default, and will be toggled back to.</center>
+        <div style={{
+          fontSize: '16px',
+          lineHeight: '150%'
+        }}>
+          Hello everyone!  My name is Thomas Cosby, a former gearbox technician turned software engineer.
+          What started out as a hobby, learning how to program very small video games in Unity, turned into a fascination of how the controlled manipulation electricty impacts our daily lives on such a fundamental level.
+          After weeks of researching and learning more about how software is made, I knew it was time to level up my life and dive deep training to be an engineer.
+          I attended the Advanced Software Engineering Immersive, Hack Reactor, to learn the techniques and technologies necessary for web-based application development.
+        </div>
       </div>
 
     ); 
--- client/components/Resume/DesktopView/DisplayBox/Slides/Default/style.js
+++ client/components/Resume/DesktopView/DisplayBox/Slides/Default/style.js
@@ -1,9 +1,16 @@
 module.exports.body = {
+  position: 'relative',
+  right: '-1000px',
   height: '100%',
   width: '100%',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  justifyContent: 'center',
-  backgroundColor: 'rgb(250, 250, 250)'
+  justifyContent: 'space-evenly',
+  backgroundColor: 'rgb(250, 250, 250)',
+  WebkitTransition: '0.2s'
+};
+
+module.exports.body_loaded = {
+  right: '0px'
 };
