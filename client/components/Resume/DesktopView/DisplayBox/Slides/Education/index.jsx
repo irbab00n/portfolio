e578ba37bc831b87317f54c8ccdcefa94f8d26c1
Thomas Cosby <cosbyts@gmail.com>
2017-12-20 15:53:25 -0800

    Added Education Slide Skeletons

--- client/components/Resume/DesktopView/DisplayBox/Slides/Education/index.jsx
+++ client/components/Resume/DesktopView/DisplayBox/Slides/Education/index.jsx
@@ -31,7 +31,81 @@ class EducationSlide extends React.Component {
           loaded && style.body_loaded
         )
       }>
-        <center>This is the Education Slide.  This slide will display when the Education Tab is clicked</center>
+
+        <div id="Educational Institution" style={{
+          height: '200px',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignContent: 'center',
+          backgroundColor: '#f2f2f2'
+        }}>
+
+          <div id="Header" style={{
+            height: '70px',
+            paddingBottom: '30px',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+            <div>
+              Image
+            </div>
+            <div>
+              Title
+            </div>
+          </div>
+
+          <div id="Content" style={{
+            height: '100px',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            Body
+          </div>
+
+        </div>
+
+        <div id="Educational Institution" style={{
+          height: '200px',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignContent: 'center',
+          backgroundColor: '#f2f2f2'
+        }}>
+
+          <div id="Header" style={{
+            height: '70px',
+            paddingBottom: '30px',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+            <div>
+              Image
+            </div>
+            <div>
+              Title
+            </div>
+          </div>
+
+          <div id="Content" style={{
+            height: '100px',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            Body
+          </div>
+
+        </div>
+
       </div>
 
     ); 
--- client/components/Resume/DesktopView/DisplayBox/Slides/Education/style.js
+++ client/components/Resume/DesktopView/DisplayBox/Slides/Education/style.js
@@ -1,12 +1,11 @@
 module.exports.body = {
   position: 'relative',
   right: '-1000px',
-  height: '100%',
+  height: '400px',
   width: '100%',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  justifyContent: 'space-evenly',
   backgroundColor: 'rgb(250, 250, 250)',
   WebkitTransition: '0.2s'
 };
