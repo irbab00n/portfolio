f7fb68c6ce13cf1c087bf4b08e75d4b1c919aa9b
Thomas Cosby <cosbyts@gmail.com>
2018-01-14 00:37:35 -0800

    Delete animationChooser_v2.js

--- animationChooser_v2.js
+++
@@ -1,5 +0,0 @@
-module.exports = (currentY, targetY, calculationFunction, animations, thresholdTest) => {
-  let animation = calculationFunction(
-    thresholdTest(currentY, currentY) ? animations.accelerate : animations.accelerate; // Defaulted to accelerate for now
-  return () => animation(currentYofThePage, targetYofThePage));
-};
