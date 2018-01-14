fc1a4363933edcdbf23a0ff533bed2b67788bb97
Thomas Cosby <cosbyts@gmail.com>
2018-01-13 23:58:30 -0800

    last work on applystyles

--- client/helpers/applyStyles.js
+++ client/helpers/applyStyles.js
@@ -1,10 +1,10 @@
 module.exports = function() {
-  let res = {};
-  for (let i = 0; i < arguments.length; ++i) {
-    if (arguments[i]) {
-      Object.assign(res, arguments[i]);
-    }
-  }
-  return res;
-  // return Object.assign({}, ...Array.prototype.slice.call(arguments));
+  // let res = {};
+  // for (let i = 0; i < arguments.length; ++i) {
+  //   if (arguments[i]) {
+  //     Object.assign(res, arguments[i]);
+  //   }
+  // }
+  // return res;
+  return Object.assign({}, ...arguments);
 };
