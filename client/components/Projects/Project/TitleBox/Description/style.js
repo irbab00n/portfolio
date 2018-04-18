762c9fe03b5a90747e7b7ce962eea55cd1bb3046
Thomas Cosby <cosbyts@gmail.com>
2018-04-18 14:08:52 -0700

    Added changes to landscape/portrait font sizes

--- client/components/Projects/Project/TitleBox/Description/style.js
+++ client/components/Projects/Project/TitleBox/Description/style.js
@@ -46,34 +46,16 @@ module.exports.description_text_fadein = {
 
 module.exports.description_text = {
   width: '90%',
-  fontSize: '1.5vh',
+  fontSize: '70%',
   lineHeight: '1.5',
   color: 'rgba(35, 35, 35, 1.0)',
   WebkitTransition: 'ease-in 0.2s'
 };
 
 module.exports.description_text_mobile_landscape = {
-  fontSize: '100%',
+  fontSize: '70%',
 };
 
 module.exports.description_text_mobile_portrait = {
-  fontSize: '100%',
-};
-
-// collapsed styles
-
-module.exports.description_text_collapsed = {
-  width: '90%',
-  fontSize: '1.2vh',
-  lineHeight: '1.5',
-  color: 'rgba(35, 35, 35, 1.0)',
-  WebkitTransition: 'ease-in 0.2s'
-};
-
-module.exports.description_text_collapsed_mobile_landscape = {
-  fontSize: '75%'
-};
-
-module.exports.description_text_collapsed_mobile_portrait = {
-  fontSize: '67%'
+  fontSize: '140%',
 };
