37a81d7bfab3d2404795ed994d5ef8c7ad58b9f3
Thomas Cosby <cosbyts@gmail.com>
2018-04-18 13:12:25 -0700

    changed index

--- client/components/Projects/Project/ToggleStrip/style.js
+++ client/components/Projects/Project/ToggleStrip/style.js
@@ -7,7 +7,7 @@ module.exports.list_body_button = {
   justifyContent: 'center',
   background: 'linear-gradient(to bottom, rgba(0, 66, 97, 0.6) 0%, rgba(0, 122, 193, 0.6) 10%, rgba(230, 230, 235, 0.1) 100%)',
   WebkitTransition: '0.5s',
-  zIndex: 2
+  zIndex: 3
 };
 
 module.exports.list_body_button_mobile = {
