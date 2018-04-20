cca09905ac2cfc9bc7ee4df44de6927008a5dda1
Thomas Cosby <cosbyts@gmail.com>
2018-04-20 16:13:38 -0700

    Adjusted project description text sizes
    - Desktop: 70% ---> 100%
    
    - Mobile Landscape: 70% ---> 100%
    
    - Mobile Portrait: 160% ---> 180%

--- client/components/Projects/Project/TitleBox/Description/style.js
+++ client/components/Projects/Project/TitleBox/Description/style.js
@@ -46,16 +46,16 @@ module.exports.description_text_fadein = {
 
 module.exports.description_text = {
   width: '90%',
-  fontSize: '70%',
+  fontSize: '100%',
   lineHeight: '1.5',
   color: 'rgba(35, 35, 35, 1.0)',
   WebkitTransition: 'ease-in 0.2s'
 };
 
 module.exports.description_text_mobile_landscape = {
-  fontSize: '70%',
+  fontSize: '100%',
 };
 
 module.exports.description_text_mobile_portrait = {
-  fontSize: '160%',
+  fontSize: '180%',
 };
