9ed65de9866a67e9293e3c7ecc3a599fd69bcbe8
Thomas Cosby <cosbyts@gmail.com>
2018-04-24 16:09:41 -0700

    Carousel images in place
    - image transition animations still need to be completed

--- client/components/Projects/Project/Carousel/index.jsx
+++ client/components/Projects/Project/Carousel/index.jsx
@@ -6,7 +6,38 @@ import style from './style';
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentIndex: 0
+    };
+    this.advanceIndex = this.advanceIndex.bind(this);
+  }
+
+  advanceIndex(action) {
+    let { currentIndex } = this.state;
+    let { project } = this.props;
+    switch(action) {
+      case 'decrement':
+        if (currentIndex === 0) {
+          this.setState({
+            currentIndex: project.pictures.length - 1
+          });
+        } else {
+          this.setState({
+            currentIndex: currentIndex - 1
+          });
+        }
+        break;
+      case 'increment':
+        if (currentIndex === project.pictures.length - 1) {
+          this.setState({
+            currentIndex: 0
+          });
+        } else {
+          this.setState({
+            currentIndex: currentIndex + 1
+          });
+        }
+    }
   }
 
   render() {
@@ -21,10 +52,12 @@ export default class Carousel extends React.Component {
       yOffset
     } = this.props;
 
+    const { currentIndex } = this.state;
+
     return (
 
       <div
-        id="carousel-box"
+        id="carousel-body"
         style={
           apply(
             style.carousel_body,
@@ -36,10 +69,135 @@ export default class Carousel extends React.Component {
           )
         }
       >
-        Carousel
+
+        <div
+          id="carousel-track"
+          style={
+            style.carousel_track
+          }
+        >
+
+          <div
+            id="track-left"
+            style={
+              style.track_left
+            }
+            onClick={() => {this.advanceIndex('decrement')}}
+          >
+            {
+              currentIndex === 0 ?
+                null :
+                <img 
+                  src={project.pictures[currentIndex - 1].link}
+                  style={{
+                    display: 'block',
+                    overflow: 'hidden',
+                    objectFit: 'scale-down',
+                    width: '100%',
+                    height: 'auto',
+                    WebkitUserSelect: 'none',
+                    khtmlUserSelect: 'none',
+                    MozUserSelect: 'none',
+                    OUserSelect: 'none',
+                    userSelect: 'none'
+                  }}
+                />
+            }
+          </div>
+
+          <div
+            id="track-current"
+            style={
+              style.track_current
+            }
+          >
+
+            <img 
+              src={project.pictures[currentIndex].link}
+              style={{
+                display: 'block',
+                objectFit: 'scale-down',
+                width: '100%',
+                height: 'auto',
+                WebkitUserSelect: 'none',
+                khtmlUserSelect: 'none',
+                MozUserSelect: 'none',
+                OUserSelect: 'none',
+                userSelect: 'none'
+              }}
+            />
+
+          </div>
+
+          <div
+            id="track-right"
+            style={
+              style.track_right
+            }
+            onClick={() => {this.advanceIndex('increment')}}
+          >
+            {
+              currentIndex === project.pictures.length - 1 ?
+                null :
+                <img 
+                  src={project.pictures[currentIndex + 1].link}
+                  style={{
+                    display: 'block',
+                    overflow: 'hidden',
+                    objectFit: 'scale-down',
+                    width: '100%',
+                    height: 'auto',
+                    WebkitUserSelect: 'none',
+                    khtmlUserSelect: 'none',
+                    MozUserSelect: 'none',
+                    OUserSelect: 'none',
+                    userSelect: 'none'
+                  }}
+                />
+            }
+          </div>
+
+        </div>
+
       </div>
 
     );
 
   }
 }
+
+
+/*
+
+For the list of pictures:
+
+we want 1 general container that will hold lists of 5 pictures.  
+
+So if we pass in 7 pictures, we're going to need to split the pictures into lists
+
+we will need to know how many lists need to be created
+
+we will need to know how far the current offset is
+
+if we click next, and the next set isn't full, we want to know how much we need to adjust the overall offset of the track by
+
+  To do this, we need to know the following:
+
+    - Size of the uniform slide width
+    - Size of the margin
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- client/components/Projects/Project/Carousel/style.js
+++ client/components/Projects/Project/Carousel/style.js
@@ -1,14 +1,16 @@
 module.exports.carousel_body = {
   height: '60%',
   width: '100%',
-  backgroundColor: 'rgba(150, 150, 150, 1.0)',
+  display: 'flex',
+  alignItems: 'center',
+  backgroundColor: 'rgba(120, 120, 120, 1.0)',
   WebkitTransition: '0.2s'
 };
 
 module.exports.carousel_body_mobile_landscape = {
   height: '90vh',
   width: '100%',
-  backgroundColor: 'rgba(150, 150, 150, 1.0)'
+  backgroundColor: 'rgba(120, 120, 120, 1.0)'
 };
 
 module.exports.carousel_body_mobile_portrait = {
@@ -32,3 +34,67 @@ module.exports.carousel_body_collapsed_mobile_portrait = {
   height: '40vh',
   width: '100%'
 };
+
+
+
+
+
+
+
+
+
+// Carousel Track Styles
+
+module.exports.carousel_track = {
+  width: '100%',
+  height: '90%',
+  display: 'flex',
+  alignItems: 'center',
+};
+
+module.exports.track_left = {
+  margin: '0 1%',
+  display: 'flex',
+  alignItems: 'center',
+  width: '18%',
+  height: '80%',
+};
+
+module.exports.track_current = {
+  margin: '0 1%',
+  display: 'flex',
+  width: '58%',
+  height: '100%',
+};
+
+module.exports.track_right = {
+  margin: '0 1%',
+  display: 'flex',
+  alignItems: 'center',
+  width: '18%',
+  height: '80%',
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
